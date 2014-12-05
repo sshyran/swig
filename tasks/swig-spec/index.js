9849a89aace1d7403995b1ce10a45ae9a5e28186
shellscape <andrew@shellscape.org>
2014-12-05 11:02:11 -0500

    refactoring common func into index.js from lib/mocha.js

--- tasks/swig-spec/index.js
+++ tasks/swig-spec/index.js
@@ -20,15 +20,32 @@ module.exports = function (gulp, swig) {
   }
 
   gulp.task('spec', function (done) {
-    var defaultSpecLib = 'mocha',
-      specLib = swig.pkg.gilt ? swig.pkg.gilt.specsLibrary || defaultSpecLib : defaultSpecLib;
+
+    var _ = require('underscore'),
+      fs = require('fs'),
+      path = require('path'),
+      glob = require('glob'),
+      sinonApiDoc = require('./lib/sinon-apidoc.js'),
+      tap = require('gulp-tap'),
+
+      defaultSpecLib = 'mocha',
+      options = {},
+      scripts = [],
+      sinonPath = path.join(path.dirname(require.resolve('sinon')), '../pkg'),
+      specFiles = [],
+      specLib = swig.pkg.gilt ? swig.pkg.gilt.specsLibrary || defaultSpecLib : defaultSpecLib,
+      specs = [],
+      servers,
+      specsPath = path.join(swig.target.path, 'public/spec/', swig.pkg.name),
+      srcPath,
+      impl;
 
     swig.log.task('Initializing Specs');
 
     try {
-      var impl = require('./lib/' + specLib.toLowerCase());
+      swig.log.info('', 'Loading ' + specLib + '...');
 
-      impl(gulp, swig, done);
+      impl = require('./lib/' + specLib.toLowerCase());
     }
     catch (e) {
       if (e.code === 'MODULE_NOT_FOUND') {
@@ -39,6 +56,58 @@ module.exports = function (gulp, swig) {
         throw e;
       }
     }
+
+    if (swig.project.type === 'webapp') {
+      srcPath = path.join(swig.target.path, 'public/js/', swig.pkg.name);
+    }
+    else {
+      srcPath = path.join(swig.target.path, 'public/js/');
+    }
+
+    swig.log.info('', 'Enumerating Dependencies...');
+
+    _.each(['browser_detect', 'json', 'modernizr', 'require', 'gilt_require'], function (module) {
+      scripts.push(path.join(__dirname, 'node_modules/internal.' + module, 'js', module + '.js'));
+    })
+
+    swig.log.info('', 'Enumerating Specs...');
+
+    // enum all of the files in the specs directory
+    specFiles = glob.sync(path.join(specsPath, '/**/*.js'));
+
+    // the file names should correspond with the name of the module used in gilt.define
+    // this is used to `require` the test files and THEN launch mocha
+    _.each(specFiles, function (file) {
+      specs.push('\'' + path.basename(file, path.extname(file)) + '\'');
+    });
+
+    swig.log.info('', 'Enumerating APIDoc Sinon Servers...');
+
+    gulp.src(path.join(swig.target.path, '/json/sinon/**/*.json'))
+      .pipe(sinonApiDoc())
+      .pipe(tap(function(file, t) {
+        try {
+          var fileServers = JSON.parse(file.contents);
+          servers = _.filter(_.union(servers, fileServers), function(a) { return typeof a !== 'undefined'; });
+        }
+        catch (e) {
+          console.log(e);
+        }
+      }))
+      .on('end', function () {
+        options = {
+          baseUrl: srcPath,
+          scripts: scripts,
+          specs: specs.join(','),
+          specsPath: specsPath,
+          specFiles: specFiles,
+          sinonPath: sinonPath,
+          servers: servers
+        };
+
+        // fire our specs implementation (jasmine, mocha, etc..)
+        impl(gulp, swig, options, done);
+      });
   });
 
 };
--- tasks/swig-spec/lib/mocha.js
+++ tasks/swig-spec/lib/mocha.js
@@ -13,18 +13,15 @@
    Brought to you by the fine folks at Gilt (http://github.com/gilt)
 */
 
-module.exports = function (gulp, swig, done) {
+module.exports = function (gulp, swig, options, done) {
 
   var _ = require('underscore'),
-    mocha = require('gulp-mocha-phantomjs'),
+    file = require('gulp-file'),
     fs = require('fs'),
     path = require('path'),
-    through = require('through2'),
+    mocha = require('gulp-mocha-phantomjs'),
     mustache = require('mustache'),
-    file = require('gulp-file'),
-    glob = require('glob'),
-    sinonApiDoc = require('./sinon-apidoc.js'),
-    tap = require('gulp-tap'),
+
     mochaPath = path.dirname(require.resolve('mocha')),
 
     // we have to reference this reporter by file path rather than name
@@ -32,76 +29,27 @@ module.exports = function (gulp, swig, done) {
     nyanPath = path.join(mochaPath, '/lib/reporters/nyan.js'),
 
     chaiPath = path.dirname(require.resolve('chai')),
-    sinonPath = path.join(path.dirname(require.resolve('sinon')), '../pkg'),
     runnerPath = path.join(__dirname, '../templates/mocha-runner.html'),
-    runner = fs.readFileSync(runnerPath, 'utf-8'),
-    specFiles = [],
-    specs = [],
-    scripts = [],
-    specsPath = path.join(swig.target.path, 'public/spec/', swig.pkg.name),
-    srcPath,
-    servers;
+    runner = fs.readFileSync(runnerPath, 'utf-8');
 
-  if (swig.project.type === 'webapp') {
-    srcPath = path.join(swig.target.path, 'public/js/', swig.pkg.name);
-  }
-  else {
-    srcPath = path.join(swig.target.path, 'public/js/');
-  }
+    swig.log.info('', 'Rendering Runner...\n');
 
-  _.each(['browser_detect', 'json', 'modernizr', 'require', 'gilt_require'], function (module) {
-    scripts.push(path.join(__dirname, '../node_modules/internal.' + module, 'js', module + '.js'));
-  })
-
-  swig.log.info('', 'Enumerating Specs...');
-
-  // enum all of the files in the specs directory
-  specFiles = glob.sync(path.join(specsPath, '/**/*.js'));
-
-  // the file names should correspond with the name of the module used in gilt.define
-  // this is used to `require` the test files and THEN launch mocha
-  _.each(specFiles, function (file) {
-    specs.push('\'' + path.basename(file, path.extname(file)) + '\'');
-  });
-
-  swig.log.info('', 'Enumerating APIDoc Sinon Servers...');
-
-  gulp.src(path.join(swig.target.path, '/json/sinon/**/*.json'))
-    .pipe(sinonApiDoc())
-    .pipe(tap(function(file, t) {
-      try {
-        var fileServers = JSON.parse(file.contents);
-        servers = _.filter(_.union(servers, fileServers), function(a) { return typeof a !== 'undefined'; });
-      }
-      catch (e) {
-        console.log(e);
-      }
-    }))
-    .on('end', function () {
-
-      swig.log.info('', 'Rendering Runner...\n');
-
-      runner = mustache.render(runner, {
-        baseUrl: srcPath,
-        scripts: scripts,
-        specs: specs.join(','),
-        specFiles: specFiles,
-        mochaPath: mochaPath,
-        chaiPath: chaiPath,
-        sinonPath: sinonPath,
-        servers: servers
-      });
-
-      swig.log.task('Running PhantomJS+Mocha');
-      swig.log('');
-
-      file('runner.html', runner, { src: true })
-        .pipe(gulp.dest(specsPath))
-        .pipe(mocha({
-          reporter: nyanPath,
-          silent: true,
-          phantomjs: { webSecurityEnabled: false }
-        }))
-        .on('end', done);
+    options = _.extend(options, {
+      mochaPath: mochaPath,
+      chaiPath: chaiPath
     });
+
+    runner = mustache.render(runner, options);
+
+    swig.log.task('Running PhantomJS+Mocha');
+    swig.log('');
+
+    file('runner.html', runner, { src: true })
+      .pipe(gulp.dest(options.specsPath))
+      .pipe(mocha({
+        reporter: nyanPath,
+        silent: true,
+        phantomjs: { webSecurityEnabled: false }
+      }))
+      .on('end', done);
 };
