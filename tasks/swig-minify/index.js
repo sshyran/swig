9a3bc82cf9f4210a150abc7e70be0926c10c7fa3
shellscape <andrew@shellscape.org>
2015-04-01 23:32:35 -0400

    adding handlebars template precompilation

--- tasks/swig-minify/index.js
+++ tasks/swig-minify/index.js
@@ -25,8 +25,8 @@ module.exports = function (gulp, swig) {
     rename = require('gulp-rename'),
     mincss = require('gulp-minify-css'),
     tap = require('gulp-tap'),
-    // filter = require('gulp-filter'),
-    // through = require('through2'),
+    rimraf = require('rimraf'),
+    handlebars = require('gulp-handlebars'),
     basePath = path.join(swig.target.path, '/public/');
 
   function renameFile (path) {
@@ -65,10 +65,49 @@ module.exports = function (gulp, swig) {
       .pipe(gulp.dest(path.dirname(glob)))
   });
 
-  gulp.task('minify', ['minify-css'], function (done) {
+  gulp.task('minify-templates', ['minify-css'], function () {
+
+  /*
+    NOTE: ui-build used handlebars@1.0.12
+          The precompile output from 1.0.0 to 2.0.0 changed significantly
+          and was not backwards compatible.
+          Handlebars is now at version 3.0.0.
+          We're using 1.0.12 and telling gulp-handlebars to use the same
+          version so that the output matches ui-build.
+          This should be updated at some point in the future.
+  */
+
+    var templatesPath = path.join(basePath, '/templates/', swig.target.name),
+      glob = path.join(templatesPath, '/**/*.handlebars');
+
+    swig.log('');
+    swig.log.task('Precompiling Handlebars Templates');
+
+    return gulp.src(glob)
+      .pipe(tap(function (file) {
+        swig.log.info('', 'Precompiling: ' + file.path.replace(templatesPath + '/', '').grey);
+      }))
+      .pipe(handlebars({
+        handlebars: require('handlebars'),
+        compilerOptions: {
+          simple: true,
+          root: 'public/templates/' + swig.target.name
+        }
+      }))
+      .pipe(rename(function (path) {
+        path.basename = path.basename.replace(templatesPath, '');
+        return path;
+      }))
+      .pipe(gulp.dest(path.join(basePath, '/js/', swig.target.name, '/templates')))
+  });
+
+  gulp.task('minify', ['minify-templates'], function (done) {
     done();
 
-    // handlebars - handlebars -s -r
+    // minify-js
+    // minify-css
+    // minify-templates
+    // done
   });
 
 };
