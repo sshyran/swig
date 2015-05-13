ee3ff766a3d1a7109ad40f31eaf2a436ca2de514
shellscape <andrew@shellscape.org>
2015-05-13 14:03:47 -0400

    update swig-stub to use inquirer

--- tools/swig-stub/index.js
+++ tools/swig-stub/index.js
@@ -21,120 +21,164 @@ module.exports = function (gulp, swig) {
     co = require('co'),
     gutil = require('gulp-util'),
     prettyjson = require('prettyjson'),
-    stubs = require('./lib/stubs');
+    stubs = require('./lib/stubs'),
+    inquirer = require('inquirer'),
 
-  gulp.task('stub', co(function * () {
+    prompts = {
+      type: 'Please enter the type of thing you\'d like to stub',
+      name: 'Please enter the name of the thing',
+      description: 'Please enter the description of the thing',
+      port: 'Please enter the port this thing should run on',
+      alias: 'If you\'d like this to have a directory alias (eg. /thing), please enter it (otherwise press return):',
+      navigation: 'Should this render the navigation/header?',
+      footer: 'Should this render the footer?',
+      continue: 'Does everything look OK?'
+    },
+    stubTypes = {
+      node: 'NodeJS Web App',
+      module: 'UI Module'
+    };
 
-    var response,
-      thingType,
-      result,
-      types = ['node'],
-      installCommand = 'npm install --tag=null --loglevel=warn 2>&1',
-      destPath,
-      data = {
-        name: 'Unknown',
-        description: 'Unknown',
-        port: 0,
-        alias: '',
-        navigation: true,
-        footer: true
-      },
-      prompts = {
-        type: 'Please enter the type of thing you\'d like to stub:',
-        name: 'Please enter the name of the thing:',
-        description: 'Please enter the description of the thing:',
-        port: 'Please enter the port this thing should run on:',
-        alias: 'If you\'d like this to have a directory alias (eg. /thing), please enter it:',
-        navigation: 'Should this render the navigation/header?',
-        footer: 'Should this render the footer?',
-        continue: 'Does everything look OK?'
-      };
+  gulp.task('stub', function (done) {
 
-    swig.log('You can stub the following types of things: ' + types.join(', ').white);
-
-    swig.log();
-    thingType = yield swig.log.prompt(swig.log.padLeft(prompts.type.white, 1));
-
-    if (!thingType || _.indexOf(types, thingType) === -1) {
-      throw new gutil.PluginError('swig-stub', 'Hey pal, you have to choose a valid type.');
+    // if we're running stub in the user root, remap the taget path
+    if (process.cwd() === process.env['HOME']) {
+      swig.target.path = path.join(swig.target.path, '/Code/web');
     }
 
-    swig.log();
-    data.name = yield swig.log.prompt(swig.log.padLeft(prompts.name.white, 1));
+    function answers (data) {
 
-    if (!data.name) {
-      throw new gutil.PluginError('swig-stub', 'Hey pal, you have to choose a name.');
-    }
+      data.type = _.findKey(stubTypes, function (value) { return value === data.type; });
 
-    swig.log();
-    data.description = yield swig.log.prompt(swig.log.padLeft(prompts.description.white, 1));
-
-    if (!data.description) {
-      throw new gutil.PluginError('swig-stub', 'Hey pal, you have to choose a name.');
-    }
-
-    swig.log();
-    data.port = yield swig.log.prompt(swig.log.padLeft(prompts.port.white, 1)) || 0;
-
-    if (!data.port) {
-      throw new gutil.PluginError('swig-stub', 'Hey pal, you have to choose a valid port > 0.');
-    }
-
-    swig.log();
-    data.alias = yield swig.log.prompt(swig.log.padLeft(prompts.alias.white, 1)) || '';
-
-    swig.log();
-    data.navigation = yield swig.log.confirm(swig.log.padLeft(prompts.navigation.white, 1));
-
-    swig.log();
-    data.footer = yield swig.log.confirm(swig.log.padLeft(prompts.footer.white, 1));
-
-    swig.log();
-    swig.log('Here\'s the data we\'ve collected:');
-    swig.log(prettyjson.render(_.extend(data, { type: thingType })));
-
-    swig.log();
-    response = yield swig.log.confirm(swig.log.padLeft(prompts.continue, 1));
-
-    if (!response) {
       swig.log();
-      swig.log.error('swig-stub', 'Allllllrighty then, thanks for playing.');
-      return;
+      swig.log('Here\'s the data we\'ve collected:');
+      swig.log(prettyjson.render(data));
+      swig.log();
+
+      inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'continue',
+          message: prompts.continue,
+          filter: filter
+        }
+      ], function (answers) {
+        if (answers.continue) {
+          return doit(data, done);
+        }
+        else {
+          swig.log();
+          swig.log.error('swig-stub', 'Maybe next time, then.');
+
+          done();
+        }
+      });
     }
 
-    destPath = path.join(swig.target.path, 'web-' + data.name);
+    function filter (value) {
+      return value.toLowerCase();
+    }
 
-    var stream = gulp
-      .src(path.join(__dirname, 'templates', data.type, '/**/*'))
-      .pipe(stubs(swig, data))
-      .pipe(gulp.dest(destPath))
-      .on('end', co(function * exit () {
+    function validate (value) {
+      return value && value.length ? true : 'Hey bub, you have to enter a value';
+    }
 
-        // ask about installing swig, npm
-        var result = yield swig.log.prompt('We\'re ready to install your NPM modules. Hit Enter to begin...');
-        swig.log();
+    function validatePort (value) {
+      return !!value && !isNaN(value) && value.toString().length > 3  && parseInt(value) > 0 ?
+              true :
+              'Hey bub, you have to enter a valid port';
+    }
 
-        swig.log.info('', 'Starting `npm install`...\n');
-        result = yield swig.exec('cd ' + destPath + '; ' + installCommand, null, {
-          stdout: function (data) {
-            swig.log(data.trim().grey);
+    function doit (data, done) {
+      var destPath = path.join(swig.target.path, data.name),
+        installCommand = 'npm install --tag=null --loglevel=warn 2>&1',
+        destPath,
+        stream;
+
+
+
+      stream = gulp
+        .src(path.join(__dirname, 'templates', data.type, '/**/*'))
+        .pipe(stubs(swig, data))
+        .pipe(gulp.dest(destPath))
+        .on('end', co(function * exit () {
+
+          // ask about installing swig, npm
+          swig.log();
+          var result = yield swig.log.prompt('We\'re ready to install your NPM modules. Press return to begin...');
+          swig.log();
+
+          swig.log.info('', 'Starting `npm install`...\n');
+          result = yield swig.exec('cd ' + destPath + '; ' + installCommand, null, {
+            stdout: function (data) {
+              swig.log(data.trim().grey);
+            }
+          });
+
+          swig.log();
+
+          if (result.stdout.indexOf('not ok') > -1){
+            swig.log.error('swig-stub', 'One or more modules failed to install from npm.\n ' +
+              swig.log.padLeft('For more info, look here: ' + path.join(destPath, 'npm_debug.log').grey, 7));
+            return;
           }
-        });
 
-        swig.log();
+          swig.log();
+          swig.log.success('', 'Your app is now setup, run `swig install` and go nuts!\n');
+          done();
 
-        if (result.stdout.indexOf('not ok') > -1){
-          swig.log.error('swig-stub', 'One or more modules failed to install from npm.\n ' +
-            swig.log.padLeft('For more info, look here: ' + path.join(destPath, 'npm_debug.log').grey, 7));
-          return;
-        }
+        }));
 
-        swig.log();
-        swig.log.success('swig-stub', 'Your app is now setup, run `swig install` and go nuts!\n');
+      return stream;
+    }
 
-      }));
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'type',
+        message: prompts.type,
+        choices: _.values(stubTypes)
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: prompts.name,
+        filter: filter,
+        validate: validate
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: prompts.description,
+        filter: filter,
+        validate: validate
+      },
+      {
+        type: 'input',
+        name: 'port',
+        message: prompts.port,
+        validate: validatePort
+      },
+      {
+        type: 'input',
+        name: 'alias',
+        message: prompts.alias,
+        filter: filter
+      },
+      {
+        type: 'confirm',
+        name: 'navigation',
+        message: prompts.navigation,
+        filter: filter
+      },
+      {
+        type: 'confirm',
+        name: 'footer',
+        message: prompts.footer,
+        filter: filter
+      }
+    ], answers);
 
-    return stream;
+  });
 
-  }));
 };
