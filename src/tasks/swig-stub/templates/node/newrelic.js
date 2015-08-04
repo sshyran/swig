6b996469fc51ea92eec068178db72eac6368f669
shellscape <andrew@shellscape.org>
2015-08-04 11:08:06 -0400

    removing newrelic.js from swig-stub templates

--- src/tasks/swig-stub/templates/node/newrelic.js
+++
@@ -1,6 +0,0 @@
-/**
- * The newrelic module still checks for this file and blows up when its missing.
- * But all of the values for the config are now set in app.chassis via
- * environment variables.
- */
-exports.config = {};
