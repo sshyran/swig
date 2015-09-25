e0c8a7611603ae45ba2f5a57a8a16c5fd71ae40b
shellscape <andrew@shellscape.org>
2015-09-25 11:18:16 -0400

    initial repo commit, moving swig-util to it's own repo

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+/*
+ ________  ___       __   ___  ________
+|\   ____\|\  \     |\  \|\  \|\   ____\
+\ \  \___|\ \  \    \ \  \ \  \ \  \___|
+ \ \_____  \ \  \  __\ \  \ \  \ \  \  ___
+  \|____|\  \ \  \|\__\_\  \ \  \ \  \|\  \
+    ____\_\  \ \____________\ \__\ \_______\
+   |\_________\|____________|\|__|\|_______|
+   \|_________|
+
+   It's delicious.
+   Brought to you by the fine folks at Gilt (http://github.com/gilt)
+*/
+
+module.exports = function (swig, gulp) {
+
+  require('./lib/email')(swig);
+  require('./lib/error')(swig);
+  require('./lib/exec')(swig);
+  require('./lib/fs')(swig);
+  require('./lib/diff')(swig);
+
+  swig.log = require('./lib/log')(swig);
+
+  // a utility to allow us to run tasks in a series, non-paralell order.
+  // we're loading this local, modded copy until the author merges a PR
+  // chalk is a dep in package.json required for this. remove that
+  // when we go back to using the npm module (if).
+  swig.seq = require('run-sequence').use(gulp);
+};
