c7a6fe5c1b5ec93fdd2a4faf44b563c9483e2ba1
Rory Haddon <rhaddon@gilt.com>
2016-08-26 14:28:24 +0100

    Add code to base gulpfile.js to autoload dev plugins

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -15,3 +15,22 @@
 */
 var gulp = require('gulp'),
   swig = require('@gilt-tech/swig')(gulp);
+
+// only require dev dependencies when in development mode,
+// or when the environment hasn't been defined.
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  if (swig.pkg.devDependencies) {
+    try {
+      Object.keys(swig.pkg.devDependencies).forEach(function(module) {
+        if (/@gilt-tech\/swig-/.test(module)) {
+          require(module)(gulp, swig);
+          console.log('Loaded: ' + module);
+        }
+      });
+    } catch (e) {
+      swig.log.error('gulpfile.js tried to require some dev dependencies that failed. Are they installed?');
+      swig.log('     - ' + e.message);
+      process.exit(1);
+    }
+  }
+}
