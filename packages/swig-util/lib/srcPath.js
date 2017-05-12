64c3e40358736e1861f1761bc9d9da95a260a118
Alan Fitzpatrick <afitzpatrick@gilt.com>
2017-05-12 17:42:33 +0100

    Fixed some linting issues

--- packages/swig-util/lib/srcPath.js
+++ packages/swig-util/lib/srcPath.js
@@ -13,24 +13,22 @@
 */
 
 module.exports = function (swig) {
-
   /**
    * @name srcPath
    * @desc Get the srcPath of the application, in order terminal params, package object, defaults
-   * @params {object} opts This is the Swig object
+   * @param {object} opts This is the Swig object
    * @returns {String}
    */
   swig.srcPath = function (opts) {
-    let
-      srcPath;
-    
-    if (opts.argv.src) {                                                                
+    let srcPath;
+
+    if (opts.argv.src) {
       srcPath = opts.argv.src;
     } else if (opts.pkg.gilt && (opts.pkg.gilt.srcPath || opts.pkg.gilt.publicPath)) { 
       srcPath = opts.pkg.gilt.srcPath || opts.pkg.gilt.publicPath;
-    } else {                                                                            
+    } else {
       srcPath = `${opts.target.path}/public`;
     }
     return srcPath;
-  }
-}; 
+  };
+};
