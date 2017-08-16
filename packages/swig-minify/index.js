56418bb47430b3ed8ac41dce65cf2d3748605c83
Conor Luddy <cluddy@gilt.com>
2017-08-16 16:23:24 +0100

    Fixing lint issue with function syntax. Updated to arrow.

--- packages/swig-minify/index.js
+++ packages/swig-minify/index.js
@@ -44,7 +44,7 @@ module.exports = function (gulp, swig) {
         loadMaps: true
       }))
       .pipe(uglify())
-      .on('error', function(err) {
+      .on('error', (err) => {
         gutil.log(gutil.colors.red('[Error]'), err.toString());
         this.emit('end');
       })
